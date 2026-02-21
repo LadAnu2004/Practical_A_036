1fbcad63d3ef278c4a2ba5989e8ec4bd075864f6
LadAnu2004 <anjalilad2112@gmail.com>
2026-02-21 21:18:25 +0530

    fisrt commit

---
+++ py4_1.py
@@ -0,0 +1,18 @@
+rows = 6
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+n = 5
+num = 1
+
+num = 1
+rows = 5
+
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+        if num == 10:
+            num = 1
+    print()
