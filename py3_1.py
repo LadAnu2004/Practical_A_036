6b78c30f084e73ce1192dd464357fcdef776c260
LadAnu2004 <anjalilad2112@gmail.com>
2026-02-21 13:20:54 +0530

    Initial commit

---
+++ py3_1.py
@@ -0,0 +1,14 @@
+#practical 3(A) 1:-
+
+rows = 6
+
+for i in range(rows, 0, -1):
+    
+        for j in range(i):
+            
+             print("*",end = " ")
+             
+        print()
+            
+       
+                    
